c753c4d5e957922f7f5ef66aed25e09126c735d2
Glitch (fcc-isqa-msg-board) <none>
2018-12-29 12:09:03 +0000

    98%

--- routes/api.js
+++ routes/api.js
@@ -20,35 +20,53 @@ module.exports = function (app) {
   
   app.route('/api/threads/:board')
     .get((req,res)=>{
-    // list recent threads  - complete
-    //I can GET an array of the most recent 10 bumped threads on the board 
-    //with only the most recent 3 replies from /api/threads/{board}. 
-    //The reported and delete_passwords fields will not be sent.
-    console.log(" get /api/threads" )
-    console.log(req.params.board)
+    // list recent threads  - incomplete
+    //I can GET an array of the most recent 10 bumped threads on the board -----------> yes
+    //with only the most recent 3 replies from /api/threads/{board}. -----------------> yes..
+    //The reported and delete_passwords fields will not be sent. --------------------->yes ...?
+    //console.log(" get /api/threads" )
+    //console.log(req.params.board)
     var boardCollection = req.params.board
     //var thread_id = req.body.thread_id
     
     //,{sort:{date:-1},limit:10},
     var msgThread = mongoose.model("msg", msgSchema, boardCollection)
-    msgThread.find((err,thread)=>{
+    
+    // need to find most 10 threades
+    //msgThread.find({},null,{sort:{bumped_on:-1},limit:10},(err,threads)=>{
+    msgThread.find({},{
+        reported: 0,
+        delete_password: 0,
+        "replies.delete_password": 0,
+        "replies.reported": 0
+      },{sort:{bumped_on:-1},limit:10},(err,threads)=>{
+    
+    
     if(err){res.send(err)}
-      var returnObj = thread
+
       //The reported and delete_passwords fields will not be sent
       // To be implemented
+      /*
+      var newArr=[]
+      returnObj = returnObj.forEach(thread=>{
+      delete thread.reported
+      delete thread.delete_password
+        console.log("================================================") 
+        console.log (thread)
+        newArr.push(thread)
+      }) 
+      */
+      
+      
       //returnObj.delete_password = undefined
       //returnObj.reported = undefined
       // should return an array
-      console.log(returnObj)
-    res.send(returnObj)
+      //console.log(threads) 
+    res.send(threads)
     })
   })
     .post((req,res)=>{
-    /// This is where we add new thread 
-    console.log(req.params) // :board 
-    console.log("=============")
-    console.log(req.query)
-    console.log(req.body)
+    
     var boardCollection = req.params.board
     
     //console.log(someMsgModel)
@@ -61,10 +79,10 @@ module.exports = function (app) {
     
     })
     
-    console.log(newMsgThread)
+    //console.log(newMsgThread)
     newMsgThread.save((err)=>{if(err) console.log(err)
                              })
-    console.log("redirecting to /b/"+ boardCollection +"/")
+    //console.log("redirecting to /b/"+ boardCollection +"/")
     res.redirect("/b/"+boardCollection + "/")
     //res.send("someething")
     
@@ -96,15 +114,21 @@ module.exports = function (app) {
     // complete but does not differentiate incorrect password or non - existing msg
     console.log(req.body)
     var boardCollection=req.params.board
-    var thread_id = req.query.thread_id
-    var delete_password = req.query.deleted_password
+    var thread_id = req.body.thread_id
+    var delete_password = req.body.delete_password
     var msgThread = mongoose.model("msg", msgSchema, boardCollection)
     
-    
+    console.log("deleting " +thread_id + " with " + delete_password)
     msgThread.findOneAndRemove({"_id":thread_id,"delete_password":delete_password},(err,thread)=>{
-      if(err){res.send(err)}
-      if (thread == null){res.send("incorrect_password")}
-      res.send("success")
+      if(err){
+        console.log("XXXX")
+        res.send(err)}
+      if (thread == null){
+        console.log("none found") 
+        res.send("incorrect password")}
+      else {
+        console.log("delete success")
+        res.send("success")} 
     })
     
     
@@ -113,9 +137,8 @@ module.exports = function (app) {
   app.route('/api/replies/:board')
     .get((req,res)=>{
     // status completed need testing to verify
-    //I can GET an array of the most recent 10 bumped threads on the board 
-    //with only the most recent 3 replies from /api/threads/{board}. 
-    //The reported and delete_passwords fields will not be sent.
+    //IIcan GET an entire thread with all it's replies 
+    //from /api/replies/{board}?thread_id={thread_id}. Also hiding the same fields.
     // get all replies of thread
     console.log(" get replies")
     console.log("for thread id= " + req.query.thread_id)
@@ -124,8 +147,16 @@ module.exports = function (app) {
     var msgThread = mongoose.model("msg", msgSchema, boardCollection)
     // {sort: {date: -1}}  {limit:10}
     //const options = { sort: { id: 1 }, limit: 2, skip: 10 }
-    msgThread.findById(thread_id,(err,thread)=>{
+    console.log(thread_id)
+    msgThread.findById(thread_id,{
+        reported: 0,
+        delete_password: 0,
+        "replies.delete_password": 0,
+        "replies.reported": 0
+      },(err,thread)=>{
     if(err){res.send(err)}
+      console.log("=================================================>")
+       
       var returnObj = thread
       //The reported and delete_passwords fields will not be sent
       // ----   To be implemented
@@ -145,10 +176,10 @@ module.exports = function (app) {
      //------------------------------------------- Incomplete
     // I can POST a reply to a thead on a specific board 
     //by passing form data text, delete_password, & thread_id to /api/replies/{board} 
-    //and it will also update the bumped_on date to the comments date.
+    //and it will also update the bumped_on date to the comments date. ----------------------------->
     //(Recomend res.redirect to thread page /b/{board}/{thread_id}) 
     //In the thread's 'replies' array will be saved _id, text, created_on, delete_password, & reported.
-    console.log("post /api/threads/")
+    console.log("post /api/replies/:board")
     var boardCollection = req.params.board
     var thread_id = req.body.thread_id
     //setup  thread infor to be passed into array
@@ -159,12 +190,14 @@ module.exports = function (app) {
       "created_on" : new Date(),
       "reported" : false
     }
-    console.log("-----------------------------------------------------------------")
-    console.log(replyInfo)
+    //console.log("-----------------------------------------------------------------")
+    //console.log(replyInfo)
     var msgThread = mongoose.model("msg", msgSchema, boardCollection)
     console.log("posting")
     // .. tbd
-    msgThread.findByIdAndUpdate(thread_id, {$addToSet:{replies:replyInfo}},(err,doc)=>{
+//    msgThread.findByIdAndUpdate(thread_id, {$addToSet:{replies:replyInfo}},(err,doc)=>{
+    msgThread.findByIdAndUpdate(thread_id, {bumped_on:new Date() , $addToSet:{replies:replyInfo}},(err,doc)=>{
+
     if(err){console.log(err)}
     res.redirect("/b/"+boardCollection + "/" + thread_id +"/")
     //(Recomend res.redirect to thread page /b/{board}/{thread_id})
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -12,8 +12,16 @@ var assert = chai.assert;
 var server = require('../server');
 
 chai.use(chaiHttp);
+ var dummyThreadsData=["Thread 01","Thread 02","Thread 03","Thread 04","Thread 05",
+                       "Thread 06","Thread 07","Thread 08","Thread 09","Thread 10", "dummy"]
+ var dummyRepliesData=["dummy reply 01", "dummy reply 02","dummy reply 03"]
 
+ 
 suite('Functional Tests', function() {
+  var thread_id1
+  var thread_id2
+  var reply_id1
+  var reply_id2
 
   suite('API ROUTING FOR /api/threads/:board', function() {
     //I can POST a thread to a specific message board 
@@ -22,18 +30,94 @@ suite('Functional Tests', function() {
     //bumped_on(date&time, starts same as created_on), reported(boolean), delete_password, & replies(array).
     suite('POST', function() {
       
-    });
-    
-    suite('GET', function() {
+      test('create 10 new threads(because we end up deleting 1 in the delete test)', function(done) {
+      chai.request(server)
+      // create > 10 dummy threads
+    //dummyThreadsData.forEach((data)=>{
+      //console.log(data)
+      chai.request(server)
+      .post('/api/threads/testSuite')
+      .send({text:"data XX",delete_password:"pass123"})
+      .end((err,res)=>{
+      assert.equal(res.status,200)
+      //thread_id1 = thread_id1
+      //})
+     
+    })
+         done()
+    })
       
+      
+    });
+    //I can GET an array of the most recent 10 bumped threads 
+    //on the board with only the most recent 3 replies from /api/threads/{board}. 
+    //The reported and delete_passwords fields will not be sent.
+    suite('GET', function() {
+      test('most recent 10 threads with most recent 3 replies each', function(done) { 
+      chai.request(server)
+      .get('/api/threads/testSuite')
+        .end((err,res)=>{
+        assert.equal(res.status,200)
+        assert.isArray(res.body) 
+        assert.isBelow(res.body.length, 11) 
+        assert.property(res.body[0], '_id');
+          assert.property(res.body[0], 'created_on');
+          assert.property(res.body[0], 'bumped_on');
+          assert.property(res.body[0], 'text');
+          assert.property(res.body[0], 'replies');
+          assert.notProperty(res.body[0], 'reported');
+          assert.notProperty(res.body[0], 'delete_password');
+          assert.isArray(res.body[0].replies);
+          assert.isBelow(res.body[0].replies.length, 4);
+        thread_id1 = res.body[0]._id
+        thread_id2 = res.body[1]._id
+
+        done()
+      })
+      
+      })
     });
     
     suite('DELETE', function() {
+            test('delete thread with good password', function(done) {
+              //console.log(thread_id1) 
+        chai.request(server)
+        .delete('/api/threads/testSuite')
+        .send({thread_id:thread_id1, delete_password:'pass123'})
+        .end(function(err, res){
+          //console.log("###")
+          assert.equal(res.status, 200);
+          //console.log("###")
+          assert.equal(res.text, 'success');
+          //console.log("###")
+          done()
+        });
+      });
+      
+      test('delete thread with bad password', function(done) {
+        chai.request(server)
+        .delete('/api/threads/testSuite')
+        .send({thread_id: thread_id2, delete_password: 'wrong'})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'incorrect password');
+          done();
+        });
+      });
       
     });
     
     suite('PUT', function() {
-      
+      test('report thread', function(done) {
+        chai.request(server)
+        .put('/api/threads/testSuite')
+        .send({report_id:thread_id2})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'success');
+          done();
+        });
+      });
     });
     
 
@@ -42,18 +126,78 @@ suite('Functional Tests', function() {
   suite('API ROUTING FOR /api/replies/:board', function() {
     
     suite('POST', function() {
-      
+      test('reply to thread', function(done) {
+        chai.request(server)
+        .post('/api/replies/testSuite') 
+        .send({thread_id: thread_id2, text:'some reply', delete_password:'pass123'})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          done(); 
+        });
+      });
     });
     
     suite('GET', function() {
-      
+      test('Get all replies for 1 thread', function(done) {
+        chai.request(server)
+        .get('/api/replies/testSuite')
+        .query({thread_id: thread_id2})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.property(res.body, '_id');
+          assert.property(res.body, 'created_on');
+          assert.property(res.body, 'bumped_on');
+          assert.property(res.body, 'text');
+          assert.property(res.body, 'replies');
+          assert.notProperty(res.body, 'delete_password');
+          assert.notProperty(res.body, 'reported');
+          assert.isArray(res.body.replies);
+          assert.notProperty(res.body.replies[0], 'delete_password');
+          assert.notProperty(res.body.replies[0], 'reported');
+          assert.equal(res.body.replies[res.body.replies.length-1].text, 'some reply');
+          
+          reply_id1 = res.body.replies[0]._id
+          done();
+        });
+      });
     });
     
     suite('PUT', function() {
-      
+            test('report reply', function(done) {
+        chai.request(server)
+        .put('/api/threads/testSuite')
+        .send({thread_id:thread_id2 ,reply_id:reply_id1})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'success');
+          done();
+        });
+      });
     });
     
     suite('DELETE', function() {
+            test('delete reply with bad password', function(done) {
+        chai.request(server)
+        .delete('/api/threads/testSuite')
+        .send({thread_id: thread_id2 ,reply_id: reply_id1, delete_password: 'wrong'})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'incorrect password');
+          done();
+        });
+      });
+      
+      test('delete reply with valid password', function(done) {
+        chai.request(server)
+        .delete('/api/threads/testSuite')
+        .send({thread_id: thread_id2 ,reply_id: reply_id1, delete_password: 'pass123'})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'success');
+          done();
+        });
+      });
+      
       
     });
     
